585b6c946b3879e7cf73158a1d0710c22e81070c
Kamil Donda <dondakamil@gmail.com>
2021-03-07 13:32:10 +0100

    Display icon and description

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.weatherapp
 
 import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.weatherapp.api.WeatherAPI
 import com.example.weatherapp.entity.Data
+import com.example.weatherapp.entity.Weather
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
@@ -20,6 +22,9 @@ class MainActivity : AppCompatActivity() {
     private val BASE_URL = "https://api.openweathermap.org/data/2.5/"
     private val CITY = "Sosnowiec,pl"
     private val API_KEY = "cd6733212b58fc4b2fd1a255c17d09c6"
+
+    private var icon: Int? = null
+    private var desc: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,7 +58,8 @@ class MainActivity : AppCompatActivity() {
     private fun fetchData(response: Response<Data>) {
         val resp = response.body()!!
 
-        val desc = resp.weather.first().description
+        fetchWeather(resp.weather.first())
+
         val temp = resp.main.temp
         val press = resp.main.pressure
         val sunrise = resp.sys.sunrise
@@ -66,5 +72,64 @@ class MainActivity : AppCompatActivity() {
         pressure.text = "$press hPa"
         description.text = desc
         city.text = CITY
+        main_icon.setImageResource(icon!!)
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun fetchWeather(weather: Weather) {
+        when(weather.icon) {
+            "01d" -> {
+                icon = R.drawable.ic_day_clear
+                desc = getString(R.string.clear_sky)
+            }
+            "01n" -> {
+                icon = R.drawable.ic_night_half_moon_clear
+                desc = getString(R.string.clear_sky)
+            }
+
+            "02d" -> {
+                icon = R.drawable.ic_day_partial_cloud
+                desc = getString(R.string.few_clouds)
+            }
+            "02n" -> {
+                icon = R.drawable.ic_night_half_moon_partial_cloud
+                desc = getString(R.string.few_clouds)
+            }
+
+            "03d","03n" -> {
+                icon = R.drawable.ic_cloudy
+                desc = getString(R.string.scattered_clouds)
+            }
+            "04d","04n" -> {
+                icon = R.drawable.ic_overcast
+                desc = getString(R.string.broken_clouds)
+            }
+            "09d","09n" -> {
+                icon = R.drawable.ic_rain
+                desc = getString(R.string.shower_rain)
+            }
+
+            "10d" -> {
+                icon = R.drawable.ic_day_rain
+                desc = getString(R.string.rain)
+            }
+            "10n" -> {
+                icon = R.drawable.ic_night_half_moon_rain
+                desc = getString(R.string.rain)
+            }
+
+            "11d","11n" -> {
+                icon = R.drawable.ic_rain_thunder
+                desc = getString(R.string.thunderstorm)
+            }
+            "13d","13n" -> {
+                icon = R.drawable.ic_snow
+                desc = getString(R.string.snow)
+            }
+            "50d","50n" -> {
+                icon = R.drawable.ic_mist
+                desc = getString(R.string.mist)
+            }
+        }
     }
 }
