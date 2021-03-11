6ae3fd8f1d944ba9e897ccdc1d39b14673acf570
Kamil Donda <dondakamil@gmail.com>
2021-03-11 15:40:56 +0100

    Put functions in VM

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainFragment.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainFragment.kt
@@ -14,18 +14,9 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.weatherapp.`view-model`.WeatherViewModel
 import com.example.weatherapp.entity.Data
-import com.example.weatherapp.entity.Weather
 import kotlinx.android.synthetic.main.fragment_main.*
-import java.text.SimpleDateFormat
-import java.util.*
-import kotlin.math.roundToInt
 
 class MainFragment : Fragment() {
-
-    private val API_KEY = "cd6733212b58fc4b2fd1a255c17d09c6"
-
-    private var icon: Int? = null
-    private var desc: String? = null
 
     private lateinit var viewModel: WeatherViewModel
 
@@ -50,7 +41,7 @@ class MainFragment : Fragment() {
         city.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 if (!query.isNullOrEmpty()) {
-                    viewModel.getWeather(query, API_KEY)
+                    viewModel.getWeather(query)
                 }
                 return true
             }
@@ -72,7 +63,7 @@ class MainFragment : Fragment() {
     private fun fetchData(response: LiveData<Data>) {
         val resp = response.value!!
 
-        fetchWeather(resp.weather.first())
+        viewModel.fetchWeather(resp.weather.first(), requireActivity())
 
         val temp = resp.main.temp
         val tempMax = resp.main.temp_max
@@ -81,85 +72,14 @@ class MainFragment : Fragment() {
         val sunriseTime = resp.sys.sunrise
         val sunsetTime = resp.sys.sunset
 
-        temperature.text = "${calcTemp(temp)}°C"
-        temperature_max.text = "${calcTemp(tempMax)}°C"
-        temperature_min.text = "${calcTemp(tempMin)}°C"
+        temperature.text = "${viewModel.calcTemp(temp)}°C"
+        temperature_max.text = "${viewModel.calcTemp(tempMax)}°C"
+        temperature_min.text = "${viewModel.calcTemp(tempMin)}°C"
         pressure.text = "$press hPa"
-        description.text = desc
-        main_icon.setImageResource(icon!!)
+        description.text = viewModel.desc
+        main_icon.setImageResource(viewModel.icon!!)
 
-        sunrise.text = convertTime(sunriseTime)
-        sunset.text = convertTime(sunsetTime)
+        sunrise.text = viewModel.convertTime(sunriseTime)
+        sunset.text = viewModel.convertTime(sunsetTime)
     }
-
-    @SuppressLint("UseCompatLoadingForDrawables")
-    private fun fetchWeather(weather: Weather) {
-        when(weather.icon) {
-            "01d" -> {
-                icon = R.drawable.ic_day_clear
-                desc = getString(R.string.clear_sky)
-            }
-            "01n" -> {
-                icon = R.drawable.ic_night_clear
-                desc = getString(R.string.clear_sky)
-            }
-
-            "02d" -> {
-                icon = R.drawable.ic_day_partial_cloud
-                desc = getString(R.string.few_clouds)
-            }
-            "02n" -> {
-                icon = R.drawable.ic_night_partial_cloud
-                desc = getString(R.string.few_clouds)
-            }
-
-            "03d","03n" -> {
-                icon = R.drawable.ic_cloudy
-                desc = getString(R.string.scattered_clouds)
-            }
-            "04d","04n" -> {
-                icon = R.drawable.ic_overcast
-                desc = getString(R.string.broken_clouds)
-            }
-            "09d","09n" -> {
-                icon = R.drawable.ic_rain
-                desc = getString(R.string.shower_rain)
-            }
-
-            "10d" -> {
-                icon = R.drawable.ic_day_rain
-                desc = getString(R.string.rain)
-            }
-            "10n" -> {
-                icon = R.drawable.ic_night_rain
-                desc = getString(R.string.rain)
-            }
-
-            "11d","11n" -> {
-                icon = R.drawable.ic_rain_thunder
-                desc = getString(R.string.thunderstorm)
-            }
-            "13d","13n" -> {
-                icon = R.drawable.ic_snow
-                desc = getString(R.string.snow)
-            }
-            "50d","50n" -> {
-                icon = R.drawable.ic_mist
-                desc = getString(R.string.mist)
-            }
-        }
-    }
-
-    @SuppressLint("SimpleDateFormat")
-    private fun convertTime(epoc: Long): String? {
-        return try {
-            val sdf = SimpleDateFormat("H:mm")
-            val netDate = Date(epoc*1000)
-            sdf.format(netDate).toString()
-        } catch (e: Exception) {
-            e.toString()
-        }
-    }
-
-    private fun calcTemp(t: Double) = (t - 273.15).roundToInt()
 }
--- WeatherApp/app/src/main/java/com/example/weatherapp/view-model/WeatherViewModel.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/view-model/WeatherViewModel.kt
@@ -1,24 +1,104 @@
 package com.example.weatherapp.`view-model`
 
+import android.annotation.SuppressLint
 import android.app.Application
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.example.weatherapp.R
 import com.example.weatherapp.entity.Data
 import com.example.weatherapp.entity.Weather
 import com.example.weatherapp.repository.WeatherRepo
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.roundToInt
 
 class WeatherViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val API_KEY = "cd6733212b58fc4b2fd1a255c17d09c6"
 
     private val _weather: MutableLiveData<Data> = MutableLiveData()
     val weather: LiveData<Data>
         get() = _weather
-
-    fun getWeather(city: String, appid: String) {
+    fun getWeather(city: String) {
         viewModelScope.launch {
-            _weather.value = WeatherRepo.getWeather(city, appid)
+            _weather.value = WeatherRepo.getWeather(city, API_KEY)
+        }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    fun convertTime(epoc: Long): String? {
+        return try {
+            val sdf = SimpleDateFormat("H:mm")
+            val netDate = Date(epoc*1000)
+            sdf.format(netDate).toString()
+        } catch (e: Exception) {
+            e.toString()
+        }
+    }
+
+    fun calcTemp(t: Double) = (t - 273.15).roundToInt()
+
+    var icon: Int? = null
+    var desc: String? = null
+    @SuppressLint("UseCompatLoadingForDrawables")
+    fun fetchWeather(weather: Weather, activity: FragmentActivity) {
+        when(weather.icon) {
+            "01d" -> {
+                icon = R.drawable.ic_day_clear
+                desc = activity.getString(R.string.clear_sky)
+            }
+            "01n" -> {
+                icon = R.drawable.ic_night_clear
+                desc = activity.getString(R.string.clear_sky)
+            }
+
+            "02d" -> {
+                icon = R.drawable.ic_day_partial_cloud
+                desc = activity.getString(R.string.few_clouds)
+            }
+            "02n" -> {
+                icon = R.drawable.ic_night_partial_cloud
+                desc = activity.getString(R.string.few_clouds)
+            }
+
+            "03d","03n" -> {
+                icon = R.drawable.ic_cloudy
+                desc = activity.getString(R.string.scattered_clouds)
+            }
+            "04d","04n" -> {
+                icon = R.drawable.ic_overcast
+                desc = activity.getString(R.string.broken_clouds)
+            }
+            "09d","09n" -> {
+                icon = R.drawable.ic_rain
+                desc = activity.getString(R.string.shower_rain)
+            }
+
+            "10d" -> {
+                icon = R.drawable.ic_day_rain
+                desc = activity.getString(R.string.rain)
+            }
+            "10n" -> {
+                icon = R.drawable.ic_night_rain
+                desc = activity.getString(R.string.rain)
+            }
+
+            "11d","11n" -> {
+                icon = R.drawable.ic_rain_thunder
+                desc = activity.getString(R.string.thunderstorm)
+            }
+            "13d","13n" -> {
+                icon = R.drawable.ic_snow
+                desc = activity.getString(R.string.snow)
+            }
+            "50d","50n" -> {
+                icon = R.drawable.ic_mist
+                desc = activity.getString(R.string.mist)
+            }
         }
     }
 }
